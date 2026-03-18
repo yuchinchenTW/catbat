61cf9c4a76129001e08c99f3138beaffe256ded3
jason_yu <jason_yu@twinhead.com.tw>
2026-03-18 15:47:55 +0800

    3

--- auto_click_start_stop.py
+++ auto_click_start_stop.py
@@ -337,10 +337,7 @@ def run_cycle(images: dict[str, str], cycle_idx: int) -> bool:
 
         time.sleep(0.1)
         wait_until_detect_then_delay_click_with_timeout(
-            images["RESULT3"], "RESULT3", delay_before_click_sec=0.2, timeout_sec=0.4, click_kwargs=strong_single
-        )   
-        wait_until_detect_then_delay_click_with_timeout(
-            images["RESULT3"], "RESULT3", delay_before_click_sec=0.1, timeout_sec=0.4, click_kwargs=strong_single
+            images["RESULT3"], "RESULT3", delay_before_click_sec=0.3, timeout_sec=0.4, click_kwargs=strong_single
         )   
 
 
@@ -348,7 +345,7 @@ def run_cycle(images: dict[str, str], cycle_idx: int) -> bool:
         gold_found = wait_until_detect_then_delay_click_with_timeout(
             images["GOLD"], "GOLD", delay_before_click_sec=0.14, timeout_sec=5.0, click_kwargs=gold_click
         )
-        
+
      
 
         if not gold_found:
